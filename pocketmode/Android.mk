23b4ea7c24b24faaa1a1e303e198177c05b72d84
Anton Hansson <hansson@google.com>
2018-02-22 17:13:00 +0100

    osborn: Set LOCAL_PRIVATE_PLATFORM_APIS
    This change sets LOCAL_SDK_VERSION for all packages where
    this is possible without breaking the build, and
    LOCAL_PRIVATE_PLATFORM_APIS := true otherwise.
    
    Setting one of these two will be made required soon, and this
    is a change in preparation for that. Not setting LOCAL_SDK_VERSION
    makes the app implicitly depend on the bootclasspath, which is
    often not required. This change effectively makes depending on
    private apis opt-in rather than opt-out.
    
    Test: make relevant package
    Bug: 73535841
    Change-Id: I5196e8c7277ff86280782e7b479a499add2e8f9f

--- pocketmode/Android.mk
+++ pocketmode/Android.mk
@@ -21,6 +21,7 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
+LOCAL_PRIVATE_PLATFORM_APIS := true
 LOCAL_PACKAGE_NAME := SmartisanPocketMode
 LOCAL_MODULE_TAGS := optional
 
