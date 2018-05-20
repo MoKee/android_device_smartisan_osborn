c8d2a727ab29e3f67ac2b824f26341bbc0cc32dc
XiNGRZ <chenxingyu92@gmail.com>
2018-05-21 00:20:41 +0800

    osborn: Read BT MAC address
    Change-Id: I6308f15a4f8a439f9e8bc169cf9e375439eb1514

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -33,6 +33,14 @@ LOCAL_SRC_FILES    := etc/init.qcom.bt.sh
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE       := init.qcom.bt.addr.sh
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.qcom.bt.addr.sh
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := hsic.control.bt.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
