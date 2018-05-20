ea32bf1cc345be3934448ccc70405128ff4fc345
XiNGRZ <chenxingyu92@gmail.com>
2018-05-20 12:37:49 +0800

    osborn: camera: Remove unused sources
    Change-Id: I5780653d7f322a80d105dcf74083c60127ddcbc0

--- camera/QCamera2/stack/Android.mk
+++ camera/QCamera2/stack/Android.mk
@@ -1,6 +1,4 @@
 LOCAL_PATH:= $(call my-dir)
 include $(LOCAL_PATH)/mm-camera-interface/Android.mk
 include $(LOCAL_PATH)/mm-jpeg-interface/Android.mk
-include $(LOCAL_PATH)/mm-jpeg-interface/test/Android.mk
-include $(LOCAL_PATH)/mm-camera-test/Android.mk
 include $(LOCAL_PATH)/mm-lib2d-interface/Android.mk
