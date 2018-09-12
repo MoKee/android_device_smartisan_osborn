a084b221dda1bfbe4bb285422c9679e653a458a3
XiNGRZ <chenxingyu92@gmail.com>
2018-09-13 00:13:23 +0800

    HAX: Build OSS Audio HAL
    Change-Id: Iffb3d86cdb48470190f05f018d078c262892d973

--- device.mk
+++ device.mk
@@ -109,6 +109,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@4.0-impl \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@4.0-impl \
+    audio.primary.sdm660 \
     audio.a2dp.default \
     audio.r_submix.default \
     audio.usb.default \
