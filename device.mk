04b67e0f9eac4a413c30a916ff40015765890b4c
Peter Cai <peter@typeblog.net>
2018-10-12 08:50:46 +0000

    osborn: audio: revert back to android.hardware.audio@2.0
    osborn needs the proprietary audio HAL to work properly and the
    proprietary HAL only works with audio 2.0
    
    Change-Id: I414581264d625e26282d21ffd550856e3beab6bb

--- device.mk
+++ device.mk
@@ -102,7 +102,7 @@ PRODUCT_CHARACTERISTICS := nosdcard
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@4.0-impl:32 \
+    android.hardware.audio@2.0-impl:32 \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@4.0-impl:32 \
     audio.a2dp.default \
