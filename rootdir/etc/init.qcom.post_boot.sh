756f98cb2dff3c1777fe55bccd5eb350ff889ab8
XiNGRZ <chenxingyu92@gmail.com>
2018-10-30 01:06:42 +0800

    osborn: Increase zram disk size to 1.5GB
    Change-Id: I2ffe0109c6eaeb52df6ac2b1c8fb77909d95d1c0

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -222,27 +222,10 @@ function 8937_sched_dcvs_hmp()
 target=`getprop ro.board.platform`
 
 function configure_zram_parameters() {
-    MemTotalStr=`cat /proc/meminfo | grep MemTotal`
-    MemTotal=${MemTotalStr:16:8}
-
-    low_ram=`getprop ro.config.low_ram`
-
-    # Zram disk - 75% for Go devices.
-    # For 512MB Go device, size = 384MB
-    # For 1GB Go device, size = 768MB
-    # Others - 512MB size
-    # And enable lz4 zram compression for Go devices
     zram_enable=`getprop ro.vendor.qti.config.zram`
     if [ "$zram_enable" == "true" ]; then
-        if [ $MemTotal -le 524288 ] && [ "$low_ram" == "true" ]; then
-            echo lz4 > /sys/block/zram0/comp_algorithm
-            echo 402653184 > /sys/block/zram0/disksize
-        elif [ $MemTotal -le 1048576 ] && [ "$low_ram" == "true" ]; then
-            echo lz4 > /sys/block/zram0/comp_algorithm
-            echo 805306368 > /sys/block/zram0/disksize
-        else
-            echo 536870912 > /sys/block/zram0/disksize
-        fi
+        echo lz4 > /sys/block/zram0/comp_algorithm
+        echo 1610612736 > /sys/block/zram0/disksize
         mkswap /dev/block/zram0
         swapon /dev/block/zram0 -p 32758
     fi
