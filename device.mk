cdee20ae0cbabc414c671a0bbc5761df89114e7d
GuaiYiHu <guaiyihu@foxmail.com>
2018-12-11 23:18:56 +0800

    osborn: proprietary: Update Radio blobs
    * Drop ims library symlinks
    * Update props for new radio blobs
    * Uprev radio and ims version
    
    Change-Id: Ib06c9682907917c875884b2f1963002e9622663c

--- device.mk
+++ device.mk
@@ -398,6 +398,9 @@ PRODUCT_PACKAGES += \
 
 # RIL
 PRODUCT_PACKAGES += \
+    android.hardware.radio@1.2 \
+    android.hardware.radio.config@1.0 \
+    android.hardware.secure_element@1.0 \
     ims-ext-common \
     librmnetctl \
     libxml2 \
