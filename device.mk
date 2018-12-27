4401ffc05c87712516d523b14d35a95656cb99b1
XiNGRZ <chenxingyu92@gmail.com>
2018-12-28 03:26:48 +0800

    osborn: Drop SOTER support until ready
    Change-Id: I020a06895240c31fe865f4e22189890704fce92a

--- device.mk
+++ device.mk
@@ -361,13 +361,6 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl:64 \
     android.hardware.sensors@1.0-service
 
-# SOTER (Fingerprint support for WeChat Payment)
-PRODUCT_PACKAGES += \
-    soter
-
-PRODUCT_BOOT_JARS += \
-    soter
-
 # Telephony
 PRODUCT_PACKAGES += \
     telephony-ext
