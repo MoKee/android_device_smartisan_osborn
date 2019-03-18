45e6a9b6652b02a433043074a96cb3dd3ba2251f
XiNGRZ <chenxingyu92@gmail.com>
2019-03-18 22:57:06 +0800

    osborn: Drop legacy Fingerprint HAL
    Change-Id: I58ae51bcde7ab893c59646fc19fe14f67d21684f

--- extract-files.sh
+++ extract-files.sh
@@ -24,11 +24,3 @@ export VENDOR=smartisan
 export DEVICE_BRINGUP_YEAR=2018
 
 ./../../$VENDOR/$DEVICE_COMMON/extract-files.sh $@
-
-MY_DIR="${BASH_SOURCE%/*}"
-if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
-MK_ROOT="$MY_DIR"/../../..
-
-BLOB_ROOT="$MK_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary
-
-sed -i 's|\x00goodixfingerprint\x00|\x00fingerprint\x00\x00\x00\x00\x00\x00\x00|' $BLOB_ROOT/vendor/lib64/hw/fingerprint.goodix.so
